5e0440a4731612e0425f4205e30fe8171f2ade3b
ye <blurrye@qq.com>
2017-07-16 14:26:53 +0800

    显示数据库数据

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from . import models
+
 
 def index(request):
-    return render(request, 'blog/index.html')
+    article = models.Article.objects.get(pk=1)
+    return render(request, 'blog/index.html', locals())
