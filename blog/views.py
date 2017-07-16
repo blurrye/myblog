19f7143b4041daf3009864e60c2ff4662adbee05
ye <blurrye@qq.com>
2017-07-16 20:06:10 +0800

    edit_page(new_page)

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
 
 from . import models
 
@@ -14,13 +13,26 @@ def article_page(request, article_id):
     return render(request, 'blog/article_page.html', locals())
 
 
-def edit_page(request):
-    return render(request, 'blog/edit_page.html')
+def edit_page(request, article_id):
+    if str(article_id) == '0':
+        return render(request, 'blog/edit_page.html')
+    else:
+        article = models.Article.objects.get(pk=article_id)
+        return render(request, 'blog/edit_page.html', locals())
 
 
+# article_id == "0", 新建文章，否则修改文章
 def edit_action(request):
     title = request.POST.get('title', 'TITLE')
     content = request.POST.get('content', 'CONTENT')
-    models.Article.objects.create(title=title, content=content)
-    articles = models.Article.objects.all()
-    return render(request, 'blog/index.html', locals())
+    article_id = request.POST.get('article_id', '0')
+    if article_id == '0':
+        models.Article.objects.create(title=title, content=content)
+        articles = models.Article.objects.all()
+        return render(request, 'blog/index.html', locals())
+    else:
+        article = models.Article.objects.get(pk=article_id)
+        article.title = title
+        article.content = content
+        article.save()
+        return render(request, 'blog/article_page.html', locals())
