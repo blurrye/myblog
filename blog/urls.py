51cfd8bde2d3d422f0746c69710d48405bad6e42
ye <blurrye@qq.com>
2017-07-16 15:28:17 +0800

    article_page

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index),
+    url(r'^article/(?P<article_id>[0-9]+)$', views.article_page),
 ]
--- blog/views.py
+++ blog/views.py
@@ -7,3 +7,8 @@ from . import models
 def index(request):
     articles = models.Article.objects.all()
     return render(request, 'blog/index.html', locals())
+
+
+def article_page(request, article_id):
+    article = models.Article.objects.get(pk=article_id)
+    return render(request, 'blog/article_page.html', locals())
