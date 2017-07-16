ab733e9ddb6275bc2edeceb3c91e5399c04fcaa1
ye <blurrye@qq.com>
2017-07-16 16:38:47 +0800

    'edit_page'

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 from . import models
@@ -12,3 +12,15 @@ def index(request):
 def article_page(request, article_id):
     article = models.Article.objects.get(pk=article_id)
     return render(request, 'blog/article_page.html', locals())
+
+
+def edit_page(request):
+    return render(request, 'blog/edit_page.html')
+
+
+def edit_action(request):
+    title = request.POST.get('title', 'TITLE')
+    content = request.POST.get('content', 'CONTENT')
+    models.Article.objects.create(title=title, content=content)
+    articles = models.Article.objects.all()
+    return render(request, 'blog/index.html', locals())
