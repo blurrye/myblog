23b737cad78e4c6fcf0753d004d979e78cd23b20
ye <blurrye@qq.com>
2017-07-16 14:58:39 +0800

    admin
    1.注册models到admin
    2.设置LANGUAGE_CODE = 'zh-hans'，即简体中文

--- myblog/settings.py
+++ myblog/settings.py
@@ -99,7 +99,11 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'en-us'
+# 简体中文
+LANGUAGE_CODE = 'zh-hans'
+# 繁体中文
+# LANGUAGE_CODE = 'zh-hant'
 
 TIME_ZONE = 'UTC'
 
